16faf4502847fc6a6b8464959e24d8bb743f15a2
hakonjunge <haakonjunge@icloud.com>
2022-08-23 15:27:09 +0200

    litt beskriving

--- august/hello_world.py
+++ august/hello_world.py
@@ -1,1 +1,9 @@
-print("hello world")
+print("hello world jeg er håkon")
+#Dette er en kommentar
+#Bruk slike for å notere og hjelpe å holde styr på enkle linjer
+"""
+bruk dette 
+for å ha 
+kommentarer over 
+flere linjer 
+"""
