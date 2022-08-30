2b9ab615844e9dae5b747654ace82fa359963f68
hakonjunge <haakonjunge@icloud.com>
2022-08-30 13:23:01 +0200

    Oppgave 1

---
+++ august/oblig_1/Oppgave1.py
@@ -0,0 +1,10 @@
+'''
+En variabel er en slags plassholder som et program skal huske, den kan brukes som en referanseverdi senere i programmet, den kan endres
+senere ettersom programmet kjøres i kronologisk rekkefølge.
+Man skriver en variabel for å gjøre det lettere å håndtere koden man skriver.
+Man kaller ofte variabler for korte og presise navn som forteller hva den skal gjøre.
+En variabel er det man setter i plassen for tall eller en «string»,
+En string er alt fra enkle symboler og bokstaver som: «x», «y», «z», men det kan også være setninger og ord.
+«Dette er et sitat som jeg skal sette inn» og «1» er begge "stringer"
+For å lage en variabel i Python skriver man ofte 'variabel = "setning eller symbol"’.
+'''
