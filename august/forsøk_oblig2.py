e3d6d9090551bd5920d9f4dbdf1595eecb1c9ca4
hakonjunge <haakonjunge@icloud.com>
2022-08-30 20:07:10 +0200

    tester

---
+++ august/forsøk_oblig2.py
@@ -0,0 +1,17 @@
+'''livets_mening = input("Hva er meningen med livet?")
+livets_mening = int(livets_mening)
+
+while livets_mening != 42:
+    if not livets_mening == 42:
+        print("FEIL!")
+        continue'''
+
+livets_mening = input("Hva er meningen med livet?")
+livets_mening = int(livets_mening)
+
+if livets_mening == 42:
+    print("Riktig, livets mening er 42!")
+
+else:
+   print("FEIL")
+   continue
