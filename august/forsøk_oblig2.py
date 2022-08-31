924eec2aad8e07d23bcf74a6e02763e8e6614cab
hakonjunge <haakonjunge@icloud.com>
2022-08-31 08:18:01 +0200

    ye

--- august/forsøk_oblig2.py
+++ august/forsøk_oblig2.py
@@ -5,7 +5,7 @@ while livets_mening != 42:
     if not livets_mening == 42:
         print("FEIL!")
         continue'''
-
+'''
 livets_mening = input("Hva er meningen med livet?")
 livets_mening = int(livets_mening)
 
@@ -14,4 +14,16 @@ if livets_mening == 42:
 
 else:
    print("FEIL")
-   continue
+   continue'''
+svar = 42
+close1 = 30
+close2 = 50
+while True:
+    livets_mening = int(input("Hva er meningen med livet?"))
+    if livets_mening == svar:
+        print("Det stemmer, meningen med livet er 42!")
+        break
+    elif close1 < livets_mening < close2:
+        print("Nærme, men feil")
+    else:
+        print("FEIL!")
