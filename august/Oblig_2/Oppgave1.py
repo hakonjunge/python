0f6268545a8a87f6056e9521a23681bc380cc220
hakonjunge <haakonjunge@icloud.com>
2022-09-01 10:35:19 +0200

    Oppgave 1

--- august/Oblig_2/Oppgave1.py
+++ august/Oblig_2/Oppgave1.py
@@ -1,4 +1,4 @@
-livets_mening = input("Hva er meningen med livet?")
+'''livets_mening = input("Hva er meningen med livet?")
 svar = 42
 livets_mening = int(livets_mening)
 close1= 30
@@ -8,4 +8,17 @@ if livets_mening == svar:
 elif close1 < livets_mening < close2:
     print("Nærme, men feil")
 else:
-    print("FEIL!")
+    print("FEIL!")'''
+
+svar = 42
+close1 = 30
+close2 = 50
+while True:
+    livets_mening = int(input("Hva er meningen med livet?"))
+    if livets_mening == svar:
+        print(f"Det stemmer, meningen med livet er {livets_mening}!")
+        break
+    elif close1 < livets_mening < close2:
+        print(f"Nærme, men {livets_mening} er feil")
+    else:
+        print("FEIL!")
---
+++ august/forelesninger/forelesning_2.py
@@ -0,0 +1,17 @@
+#int, float og bool, (None)
+
+int_variable = 42
+print(int_variable)
+
+float_variable = 3.1415926
+print(float_variable)
+
+bool_true = True
+bool_false = False
+print(bool_true)
+print(bool_false)
+
+no_value = None
+print(no_value)
+
+#int(), float(), str(), bool()
